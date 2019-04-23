dfa91ce20dcddd06d12799d0021677b4b817866e
Vikas Kulwal <vikaskulwal@gmail.com>
2019-04-24 00:59:48 +0530

    second commit

--- app/src/main/java/matrimonial/assignment/com/matrimonialassignment/LoginActivity.java
+++ app/src/main/java/matrimonial/assignment/com/matrimonialassignment/LoginActivity.java
@@ -1,13 +1,30 @@
 package matrimonial.assignment.com.matrimonialassignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView login_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_layout);
+        init();
+    }
+
+
+    private void init() {
+        login_tv = findViewById(R.id.login_tv);
+        login_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, DashBoardActivity.class) ;
+                startActivity(intent);
+            }
+        });
     }
 }
